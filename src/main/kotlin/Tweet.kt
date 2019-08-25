dc17145544fad486f45b689693f9a4f64eb8d4d4
gabrielgatu <gabriel.dny@gmail.com>
2019-08-25 20:27:06 +0200

    Initial commit

---
+++ src/main/kotlin/Tweet.kt
@@ -0,0 +1,15 @@
+import java.util.*
+
+data class Tweet(
+    val tweetId: String,
+    val text: String,
+    val date: Date,
+    val replies: Int,
+    val retweets: Int,
+    val likes: Int,
+    val isRetweet: Boolean,
+    val hashtags: List<String>,
+    val urls: List<String>,
+    val photos: List<String>,
+    val videoIds: List<String>
+)
